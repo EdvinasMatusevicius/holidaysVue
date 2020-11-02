6fd91e0f2cbc320e929e0261fe2e2f37dcaa4a73
Edvinas <edvinas2121@hotmail.com>
2020-11-02 22:04:55 +0200

    views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="top">
+      <select class="custom-select custom-select-lg mb-3">
+        <option selected>Select country</option>
+        <option value="1">One</option>
+      </select>
+      <div class="col-md-3 mb-3">
+        <input type="text" class="form-control" id="validationDefault05" placeholder="Year" required>
+      </div>
+      <router-link 
+        :to="'/holidays/'"
+        tag="button"
+        :disabled="canSearch"
+      >Get holidays</router-link>
+    </div> 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import api from '@/api/api.js';
 export default {
   name: 'Home',
+  props:[],
   components: {
-    HelloWorld
+  },
+  data(){
+      return{
+          countriesList: [],
+          canSearch: false
+      }
+  },
+  created(){
+    this.getCountries();
+  },
+  methods:{
+    async getCountries(){
+      await api.getCountries((countriesArr)=>{
+        this.countriesList = countriesArr;
+      },
+      (error)=>{
+        console.log(error)
+      });
+    }
   }
 }
 </script>
+
+<style scoped>
+  .getVals{
+    display: inline-block;
+    padding: 5px 1rem;
+    margin-left:2rem;
+    background:rgb(42, 165, 122);
+    color: white;
+    cursor: pointer;
+    ;
+  }
+  .top{
+    margin-top: 2rem;
+  }
+</style>
