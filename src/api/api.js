ea8d0355fd4565b11727c65aa81d14cab0f29022
Edvinas <edvinas2121@hotmail.com>
2020-11-02 21:20:09 +0200

    created get countries and holidays api calls

---
+++ src/api/api.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+
+const API_URL = 'http://holiday-api.serverpi.ddns.me/api/';
+
+export default {
+
+  getCountries: async(success,failure)=>{
+    try {
+      const response = await api().get(API_URL + 'countries');
+      success(response.data.message);
+    } catch (error) {
+      failure(error);
+    }
+  },
+  getHolidays: async(requestData,success,failure)=>{
+    try {
+      paramsUrl = `${requestData.countryCode}/${requestData.year}`+ (requestData.region ? `/${requestData.region}` : ''); 
+      const response = await api().get(API_URL + 'holidays/'+paramsUrl);
+      success(response.data.message);
+    } catch (error) {
+      failure(error);
+    }
+  },
+
+}
+//--------------------------------------------------------------------------------------------AXIOS HEADER CONFIGURATION
+function api() {
+    return axios.create({
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+}
+
